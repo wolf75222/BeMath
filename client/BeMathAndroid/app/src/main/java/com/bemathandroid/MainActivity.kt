add8649915ae1db1e182263327ab8b67414bdbc8
remirhr <remirahir25@gmail.com>
2023-10-11 17:05:30 +0200

    Figma

--- client/BeMathAndroid/app/src/main/java/com/bemathandroid/MainActivity.kt
+++ client/BeMathAndroid/app/src/main/java/com/bemathandroid/MainActivity.kt
@@ -1,13 +1,13 @@
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bemathandroid.component6.Component6
+/*import com.bemathandroid.component6.Component6*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Component6(/* les paramètres, si nécessaires */)
+          /*  Component6(/* les paramètres, si nécessaires */)*/
         }
     }
 }
